9d063cd1754f8ace533131d11408951f33f065a2
Vinay <kalagavinay@github.com>
2021-06-16 22:05:33 +0530

    my new test email project

---
+++ test-send-email/src/test/java/com/vinay/testsendemail/TestSendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.testsendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
